742369612db7cb23e98afa4ec9b845b730c77834
roadmonster <roadmonster1989@gmail.com>
2020-03-14 22:57:33 -0700

    march 14 push

--- ParallelizedDijkstra.java
+++ ParallelizedDijkstra.java
@@ -1,9 +1,15 @@
 import java.util.*;
+import java.lang.Math;
 
 class ParallelizedDijkstra{
     
     private static final int MAX_INF = Integer.MAX_VALUE;
     private static final int THREAD_N = 4;
+    private int[] shortestDis = new int[THREAD_N];
+    private int[] closestVertex = new int[THREAD_N];
+
+    
+
 
     class Edge{
         int s, e, dis;
@@ -57,12 +63,21 @@ class ParallelizedDijkstra{
     private int dijkstra(int N, int start, int end, Map<Integer, List<Edge>> graph){
         int[] dis = new int[N];
         boolean[] selected = new boolean[N];
+
         for(int i = 0; i < N; i ++){
             dis[i] = MAX_INF;
             selected[i] = false;
         }
+
+        for(int i = 0; i < THREAD_N; i++){
+            shortestDis[i] = MAX_INF;
+            closestVertex[i] = -1;
+        }
+
         dis[start] = 0;
         for(int i = 0; i < N; i ++){
+            
+            /**
             int selectedVertex = -1, minDis = MAX_INF;
             for(int j = 0; j < N; j ++){
                 if(!selected[j] && minDis > dis[j]){
@@ -73,6 +88,16 @@ class ParallelizedDijkstra{
             if(selectedVertex == -1){
                 break;
             }
+            **/
+
+            // threaded version
+
+            int[] result = findMinDistance(N, selected, dis);
+            int selectedVertex = result[0];
+            int minDis = result[1];
+
+            System.out.println(selectedVertex);
+            System.out.println(minDis);
             System.out.printf("[i = %d] vertex = %d, minDis = %d\n", i, selectedVertex, minDis);
             dis[selectedVertex] = minDis;
             selected[selectedVertex] = true;
@@ -109,14 +134,85 @@ class ParallelizedDijkstra{
         return dis[end];
     }
 
+    /**
+     * Helper method to find the vertex with the shortest distance from the start
+     */
+    private int[] findMinDistance(int N, boolean[] selected, int[]dis){
+
+
+        int minDis = MAX_INF;
+        int selectedVertex = -1;
+
+        //the amount of data for my thread
+        
+        int my_chunk = N / THREAD_N + 1;
+        
+        int startIndex = 0;
+        int thread_index = 0;
+        List<Thread> threads = new ArrayList<>();
+
+        while(startIndex < N){
+
+            CloestVertexFinder finder = new CloestVertexFinder(startIndex,  Math.min(startIndex + my_chunk, N), thread_index, selected, dis);
+            startIndex += my_chunk;
+            thread_index += 1;
+            
+            Thread t = new Thread(finder);
+            threads.add(t);
+            t.start();
+        }
+
+        for(Thread t : threads){
+            try{
+                t.join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        //doing the reduce, to find the shortest distance and its vertext
+        for(int i = 0; i < THREAD_N; i++){
+            if(shortestDis[i] < minDis){
+                minDis = shortestDis[i];
+                selectedVertex = closestVertex[i];
+            }
+
+        }
+
+        //put the shortest distance and vertex into array to return
+        int[]array = {selectedVertex, minDis};
+
+        return array;
+    }
+
     /*
     * this class is used in the first parallel part, which can help you find the closest vertex to the start vertex
     */
-    class findCloestVertex implements Runnable{
-        //TODO
+    class CloestVertexFinder implements Runnable{
+        int startIndex, endIndex, thread_index;
+        boolean[]selected;
+        int[] dis;
+
+
+        public CloestVertexFinder(int startIndex, int endIndex, int thread_index, boolean[] selected, int[]dis){
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+            this.thread_index = thread_index;
+            this.selected = selected;
+            this.dis = dis;
+        }
+
+
         @Override
         public void run(){
-            //TODO
+            int min = Integer.MAX_VALUE, min_index = -1; 
+
+            for(int j = startIndex; j < endIndex; j ++){
+                if (selected[j] == false && dis[j] <= shortestDis[thread_index]) {
+                    shortestDis[thread_index] = dis[j]; 
+                    closestVertex[thread_index] = j; 
+                }
+            }
         }
     }
 
@@ -156,8 +252,9 @@ class ParallelizedDijkstra{
 
     public static void main(String[] args){
         ParallelizedDijkstra obj = new ParallelizedDijkstra();
-        String filePath = "/Users/xxx0624/Dijkstra/testcase/TestData1.txt";
+        String filePath = "/Users/haoli/Downloads/Dijkstra-master/testcase/TestData1.txt";
         List<List<Integer>> data = Utils.readData(filePath);
+        
         if(data.size() < 2){
             return ;
         }
